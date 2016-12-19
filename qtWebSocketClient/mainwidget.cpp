61a85674089d1149d1679acef97886c00e47453a
alex.dwgpl <alex.dwgpl@t10.pw>
2016-12-19 08:48:43 +0200

    fix conflicts

--- qtWebSocketClient/mainwidget.cpp
+++ qtWebSocketClient/mainwidget.cpp
@@ -7,12 +7,6 @@ MainWidget::MainWidget()
     mClient        = new Client(this);
     mCompressor    = new Compressor(this);
 
-<<<<<<< HEAD
-    addTab(m_dialog, "Dialog");
-    addTab(m_options, "Options");
-
-    connect()
-=======
     addTab(mDialog, "Dialog");
     addTab(mOptions, "Options");
 }
@@ -25,6 +19,5 @@ void MainWidget::slotSend(QString message, bool needToCompress)
         message = QString(bytes);
     }
     mClient->getSocket()->sendTextMessage(message);
->>>>>>> 6ca05ff062614b0ea4d27d8bde255115e69c7d2d
 }
 
